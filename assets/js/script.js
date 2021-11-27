0a99d6df509b5594e62e3ab5b816a92665ef98ff
Ian Linton <ilinton2571@gmail.com>
2021-11-27 12:17:31 -0700

    added function to add tasks to list

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 var buttonEl = document.querySelector("#save-task");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-// click event for the add button
-buttonEl.addEventListener('click', function() {
+// function to create a new task
+var createTaskHandler = function() {
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
-})
+}
+
+// on button click, creates a new task
+buttonEl.addEventListener('click', createTaskHandler);
