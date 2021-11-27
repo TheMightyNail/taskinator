86104b74f6adfab0b8cdd3d5d03c7a53ea0c96fb
Ian Linton <ilinton2571@gmail.com>
2021-11-27 13:07:28 -0700

    added task drop down functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // function to create a new task
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+    event.preventDefault();
+    
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
@@ -10,4 +12,4 @@ var createTaskHandler = function() {
 }
 
 // on button click, creates a new task
-buttonEl.addEventListener('click', createTaskHandler);
+formEl.addEventListener('submit', createTaskHandler);
